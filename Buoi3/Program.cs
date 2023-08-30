0eaac0eed87614b9497d87dbac24088c2de474d4
= <=>
2023-08-30 14:25:58 +0700

    Add file buoi3

---
+++ Buoi3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToaDo goc = new ToaDo() ;
+            ToaDo B = new ToaDo(12, 16, "B");
+            Console.WriteLine("Goc: " + goc.ToString());
+            Console.WriteLine("Diem B: " + B.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Buoi3/ToaDo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi3
+{
+    class ToaDo
+    {
+        private int toadoX;
+        private int toadoY;
+        private string tentoaDo;
+
+        public int ToaDoX { get{return toadoX;} set{toadoX = value;} }
+        public int ToaDoY { get{return toadoY;} set{toadoY = value;} }
+        public string TenToaDo { get { return tentoaDo; } set { tentoaDo = value; } }
+        public ToaDo()
+        {
+            toadoX = 0;
+            toadoY = 0;
+            tentoaDo = "O";
+        }
+        public ToaDo(int tx, int ty, string tentd)
+        {
+            this.toadoX = tx;
+            this.toadoY = ty;
+            this.tentoaDo = tentd;
+        }
+        public override string ToString()
+        {
+            return "ToaDo: " + tentoaDo + "(" + toadoX + " ," + toadoY + ")";
+        }
+    }
+}
